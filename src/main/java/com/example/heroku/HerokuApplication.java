9f663c1b0d2a1284bfa75f45e6c59879b612f825
jerryliu <jiayuliu@u.boisestate.edu>
2021-11-23 12:57:54 -0700

    mesa

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -61,8 +61,8 @@ public class HerokuApplication {
       Statement stmt = connection.createStatement();
       //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp, name varchar(30))");
       //stmt.executeUpdate("INSERT INTO ticks VALUES (now(), '" + getRandomString() + "')");
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp, name varchar(30))");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now(), '" + getRandomString() + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
